86b340b5d6d79aab7379aa3fa4c35f197d125061
Pranik Chainani <pranikc@gmail.com>
2021-07-19 22:21:17 -0400

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,12 +4,17 @@ class AuthorsController < ApplicationController
   end
 
   def new
+    @author = Author.new
   end
 
   def create
-    @author = Author.create!(author_params)
+    @author = Author.create(author_params)
 
-    redirect_to author_path(@author)
+    if @author.valid?
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
   end
 
   private
